d72f8b419a6d266eaad547111d6a03c9ae9dc55d
Henry Weickert <henry.weickert@mackevision.com>
2015-10-20 12:06:44 +0200

    support for root git directories added

--- pip_repo_manager/_multi_git.py
+++ pip_repo_manager/_multi_git.py
@@ -7,9 +7,10 @@ from . _git_repo_status import GitRepoStatus
 
 
 class MultiGit( object ):
-    def __init__( self, root_dp, git_executable_fp="git" ):
+    def __init__( self, root_dp, git_executable_fp="git", include_root_git_dp=True ):
         self._root_dp = root_dp
         self._git_executable_fp = git_executable_fp
+        self._include_root_git_dp = include_root_git_dp
 
 
     def gen_statii( self ):
@@ -30,6 +31,9 @@ class MultiGit( object ):
 
 
     def _gen_dps( self, root_dp ):
+        if self._include_root_git_dp:
+            yield os.path.join( self._root_dp, ".git" )
+
         for n in os.listdir( root_dp ):
             if not os.path.isdir( root_dp+"/"+n ):
                 continue
--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ import setuptools
 setuptools.setup(
     name="pip_repo_manager",
     packages=['pip_repo_manager'],
-    version="0.1.1",
+    version="0.1.2",
     install_requires=[]
 )
